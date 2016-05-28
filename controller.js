2a066a3d2046fc4846199dfb9b58e44cb04defab
Ryan McCahan <ryan@mccahan.net>
2016-05-28 14:33:16 -0600

    Allow sprinkler date scheduling support

--- controller.js
+++ controller.js
@@ -1,14 +1,27 @@
 #!/usr/local/bin/node
 
-require('console-stamp')(console, '[HH:MM:ss.l]');
+require('console-stamp')(console, 'isoDateTime');
 
+var apiKey = '4905434f280341a7ac1a3818060f6704';
 var net = require('net');
+var rp = require('request-promise');
 var Blynk = require('blynk-library');
-var blynk = new Blynk.Blynk('4905434f280341a7ac1a3818060f6704', options = { connector: new Blynk.TcpClient( { addr: 'localhost'} ) });
+var blynk = new Blynk.Blynk(apiKey, options = { connector: new Blynk.TcpClient( { addr: 'localhost'} ) });
 
 var devices = {};
 devices.garage = { address: null, last: null, pins: { 'openState': null }};
 devices.sprinklers = { address: null, last: null };
+
+// Read pin function
+blynk.readPin = function(pin) {
+  var options = {
+    uri: 'http://127.0.0.1:8080/' + apiKey + '/pin/V' + pin,
+    transform: function(body, response, resolveWithFullResponse) {
+      return JSON.parse(body)[0];
+    }
+  };
+  return rp(options);
+};
 
 // Listening Server
 var registrationString = /DEVICE: (.*) - (.*)/;
@@ -32,7 +45,7 @@ var server = net.createServer(function(socket) {
     deviceData = messageString.exec(body);
     if (deviceData !== null) {
       if (devices[deviceData[1]].onMessage !== null)
-	devices[deviceData[1]].onMessage(deviceData[2], socket);
+        devices[deviceData[1]].onMessage(deviceData[2], socket);
     }
   });
 }).listen(5000);
@@ -91,13 +104,13 @@ v5.on('write', zoneHandler(3));
 
 var sendSprinklerMessage = function(message) {
   console.log("Send sprinkler message " + message);
-  var client = new net.Socket();
+  /*var client = new net.Socket();
   client.on('data', function(data){
     client.destroy();
   });
   client.connect(80, devices.sprinklers.address, function() {
     client.write(message);
-  });
+  });*/
 };
 var runSprinklerCycle = function(zone, duration) {
   var message;
@@ -106,40 +119,72 @@ var runSprinklerCycle = function(zone, duration) {
     sendSprinklerMessage('ZONE' + zone + 'OFF');
   }, duration*1000);
 };
+var runFullSprinklerCycle = function() {
+  // Check to see the duration of the cycle we should run
+  blynk.readPin(15).then(function(duration){
+    console.log("Running sprinklers with", duration, "minute duration");
+    console.log("Starting sprinker cycle");
+    runSprinklerCycle(0, duration*60);
+    var offset = duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(1, duration*60);
+    }, offset*1000);
+    offset += duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(2, duration*60);
+    }, offset*1000);
+    offset += duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(3, duration*60);
+    }, offset*1000);
+
+    // Wait an hour between each cycle for run-and-soak technique
+    offset += 60*60;
+    setTimeout(function(){
+      runSprinklerCycle(0, duration*60);
+    }, offset*1000);
+    offset += duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(1, duration*60);
+    }, offset*1000);
+    offset += duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(2, duration*60);
+    }, offset*1000);
+    offset += duration*60+10;
+    setTimeout(function(){
+      runSprinklerCycle(3, duration*60);
+    }, offset*1000);
+  });
+};
 
 var v6 = new blynk.VirtualPin(6);
 v6.on('write', function(param) {
   if (param[0] == '1') {
-    console.log("Starting sprinker cycle");
-    runSprinklerCycle(0, 3*60);
-    var offset = 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(1, 3*60);
-    }, offset*1000);
-    offset += 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(2, 3*60);
-    }, offset*1000);
-    offset += 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(3, 3*60);
-    }, offset*1000);
-
-    offset += 20*60;
-    setTimeout(function(){
-      runSprinklerCycle(0, 3*60);
-    }, offset*1000);
-    offset += 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(1, 3*60);
-    }, offset*1000);
-    offset += 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(2, 3*60);
-    }, offset*1000);
-    offset += 3*60+10;
-    setTimeout(function(){
-      runSprinklerCycle(3, 3*60);
-    }, offset*1000);
+    // Check to see if we're supposed to water today
+    var date = new Date();
+    var pinID = date.getDay() + 8;
+    blynk.readPin(pinID).then(function(shouldWater) {
+      if (shouldWater == "1") {
+        console.log("Sprinklers are scheduled to run today");
+        runFullSprinklerCycle();
+      }
+    });
   }
 });
+
+// V7 - Manual Sprinkler Cycle Button
+var v7 = new blynk.VirtualPin(7);
+v7.on('write', function(param) {
+  if (param[0] == '1') {
+    runFullSprinklerCycle();
+  }
+});
+
+// V8 - Sunday
+// V9 - Monday
+// V10 - Tuesday
+// V11 - Wednesday
+// V12 - Thursday
+// V13 - Friday
+// V14 - Saturday
